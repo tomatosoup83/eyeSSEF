8496927bf649b2969274c41c0c5bb3e63536e298
honganh21 <honganh6c1@gmail.com>
2025-12-19 11:33:44 +0700

    i planned some stuff

---
+++ videoImplement/main.py
@@ -0,0 +1,9 @@
+# thing to split the video into multiple files??
+
+def videoToImages(video):
+    # 1. clear the images folder
+    # 2. convert the video into multiple .bmp files and store it in the tempImages folder
+    # 3. turn images ito grayscale (actually i think this is part of the algorithm but meh)
+
+    
+    pass
